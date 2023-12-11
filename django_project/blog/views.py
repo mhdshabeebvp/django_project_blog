55c086c892ff4586faa34f06afe6eccec1f207a7
mhdshabeebvp <sshabeeb33@gmail.com>
2023-12-11 16:34:21 +0530

    add title section on both home and about pages

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "mohammed shabeeb",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "August 26, 2023",
+    },
+    {
+        "author": "mohammed aslam",
+        "title": "Blog post 2",
+        "content": "second post content",
+        "date_posted": "August 29, 2023",
+    },
+]
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})

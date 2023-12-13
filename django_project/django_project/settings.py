dcb4381f8ae7468e38800089b9c810cb3c4951f8
mhdshabeebvp <sshabeeb33@gmail.com>
2023-12-13 17:30:33 +0530

    create user registration page, create new app users user registration ,install django-crispy-forms

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,13 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "blog.apps.BlogConfig",
+    "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "blog/static")]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

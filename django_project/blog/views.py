72da89a255644608ccaf24180cb4d6cd1558439a
mhdshabeebvp <sshabeeb33@gmail.com>
2023-12-13 12:23:28 +0530

    create dummy database and create some posts, remove the previously created dummy data from views.py , modify the time formate from home.html

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        "author": "mohammed shabeeb",
-        "title": "Blog post 1",
-        "content": "First post content",
-        "date_posted": "August 26, 2023",
-    },
-    {
-        "author": "mohammed aslam",
-        "title": "Blog post 2",
-        "content": "second post content",
-        "date_posted": "August 29, 2023",
-    },
-]
-
-
+# import models.y (database) we can remove the dummy data from here
+from .models import Post
 # Create your views here.
+
+
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}  # add the Post.objects.all() from databases
     return render(request, "blog/home.html", context)
 
 

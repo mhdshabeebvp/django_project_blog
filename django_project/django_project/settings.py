2dfe2f71c292424b983f409ac4e4d39a21fe7ec0
mhdshabeebvp <sshabeeb33@gmail.com>
2023-12-24 13:34:32 +0530

    user to updates their profile informations and they can also updates their profile pictures

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+  
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdatesForm
 from .models import Profile  # Assuming you have a Profile model
 
 
@@ -25,7 +25,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    # Retrieve or create the profile for the current user
-    profile, created = Profile.objects.get_or_create(user=request.user)
+    if request.method == "POST":
+        # Retrieve or create the profile for the current user
+        profile, created = Profile.objects.get_or_create(user=request.user)
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdatesForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Your account has been updated!")
+            return redirect("profile")
 
-    return render(request, "users/profile.html", {"profile": profile})
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdatesForm(instance=request.user.profile)
+
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "users/profile.html", context)
